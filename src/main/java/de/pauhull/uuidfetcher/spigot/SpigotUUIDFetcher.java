368c0c3d30a4fdc1b9c9d028f20cfb3b9b8121dc
pauhull <pauhull3@gmail.com>
2018-12-12 21:43:25 +0100

    Added Profile so double fetching isnt needed

--- src/main/java/de/pauhull/uuidfetcher/spigot/SpigotUUIDFetcher.java
+++ src/main/java/de/pauhull/uuidfetcher/spigot/SpigotUUIDFetcher.java
@@ -8,6 +8,7 @@ import de.pauhull.uuidfetcher.common.communication.message.NameRequestMessage;
 import de.pauhull.uuidfetcher.common.communication.message.ResponseMessage;
 import de.pauhull.uuidfetcher.common.communication.message.UUIDRequestMessage;
 import de.pauhull.uuidfetcher.common.fetcher.UUIDFetcher;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -28,61 +29,74 @@ public class SpigotUUIDFetcher implements MessageListener, UUIDFetcher {
     private JavaPlugin plugin;
 
     @Getter
-    private Map<String, List<Consumer<UUID>>> requestedUUIDs = new HashMap<>();
+    private Map<String, List<Consumer<Profile>>> requestedProfilesByName = new HashMap<>();
 
     @Getter
-    private Map<UUID, List<Consumer<String>>> requestedNames = new HashMap<>();
+    private Map<UUID, List<Consumer<Profile>>> requestedProfilesByUUID = new HashMap<>();
 
     public SpigotUUIDFetcher(JavaPlugin plugin) {
         this.plugin = plugin;
         TimoCloudAPI.getMessageAPI().registerMessageListener(this);
     }
 
-    @Override
-    public void fetchUUIDAsync(String playerName, Consumer<UUID> consumer) {
+    public void fetchProfileAsync(String playerName, Consumer<Profile> consumer) {
 
         Player player = Bukkit.getPlayer(playerName);
         if (player != null) {
-            consumer.accept(player.getUniqueId());
+            consumer.accept(new Profile(player.getName(), player.getUniqueId()));
             return;
         }
 
         UUIDRequestMessage requestMessage = new UUIDRequestMessage(playerName);
         requestMessage.sendToProxy("Proxy");
 
-        if (requestedUUIDs.containsKey(playerName.toUpperCase())) {
-            requestedUUIDs.get(playerName.toUpperCase()).add(consumer);
+        if (requestedProfilesByName.containsKey(playerName.toUpperCase())) {
+            requestedProfilesByName.get(playerName.toUpperCase()).add(consumer);
         } else {
-            List<Consumer<UUID>> consumers = new ArrayList<>();
+            List<Consumer<Profile>> consumers = new ArrayList<>();
             consumers.add(consumer);
-            requestedUUIDs.put(playerName.toUpperCase(), consumers);
+            requestedProfilesByName.put(playerName.toUpperCase(), consumers);
         }
     }
 
-    @Override
-    public void fetchNameAsync(UUID uuid, Consumer<String> consumer) {
+    public void fetchProfileAsync(UUID uuid, Consumer<Profile> consumer) {
 
         Player player = Bukkit.getPlayer(uuid);
         if (player != null) {
-            consumer.accept(player.getName());
+            consumer.accept(new Profile(player.getName(), player.getUniqueId()));
             return;
         }
 
         NameRequestMessage requestMessage = new NameRequestMessage(uuid);
         requestMessage.sendToProxy("Proxy");
 
-        if (requestedNames.containsKey(uuid)) {
-            requestedNames.get(uuid).add(consumer);
+        if (requestedProfilesByUUID.containsKey(uuid)) {
+            requestedProfilesByUUID.get(uuid).add(consumer);
         } else {
-            List<Consumer<String>> consumers = new ArrayList<>();
+            List<Consumer<Profile>> consumers = new ArrayList<>();
             consumers.add(consumer);
-            requestedNames.put(uuid, consumers);
+            requestedProfilesByUUID.put(uuid, consumers);
         }
     }
 
+    @Deprecated
+    @Override
+    public void fetchUUIDAsync(String playerName, Consumer<UUID> consumer) {
+        fetchProfileAsync(playerName, profile -> consumer.accept(profile.getUuid()));
+    }
+
+    @Deprecated
+    @Override
+    public void fetchNameAsync(UUID uuid, Consumer<String> consumer) {
+        fetchProfileAsync(uuid, profile -> consumer.accept(profile.getName()));
+    }
+
+    @Deprecated
     @Override
     public void getNameCaseSensitive(String name, Consumer<String> consumer) {
-        fetchUUIDAsync(name, uuid -> fetchNameAsync(uuid, consumer));
+        fetchProfileAsync(name, profile -> {
+            consumer.accept(profile.getName());
+        });
     }
 
     @Override
@@ -94,24 +108,36 @@ public class SpigotUUIDFetcher implements MessageListener, UUIDFetcher {
             ResponseMessage responseMessage = new ResponseMessage(message);
             String player = responseMessage.getPlayerName();
             UUID uuid = responseMessage.getUuid();
+            Profile profile = new Profile(player, uuid);
 
-            if (player != null && requestedUUIDs.containsKey(player.toUpperCase())) {
-                for (Consumer<UUID> consumer : requestedUUIDs.get(player.toUpperCase())) {
-                    consumer.accept(uuid);
+            if (player != null && requestedProfilesByName.containsKey(player.toUpperCase())) {
+                for (Consumer<Profile> consumer : requestedProfilesByName.get(player.toUpperCase())) {
+                    consumer.accept(profile);
                 }
 
-                requestedUUIDs.remove(player.toUpperCase());
+                requestedProfilesByName.remove(player.toUpperCase());
             }
 
-            if (uuid != null && requestedNames.containsKey(uuid)) {
-                for (Consumer<String> consumer : requestedNames.get(uuid)) {
-                    consumer.accept(player);
+            if (uuid != null && requestedProfilesByUUID.containsKey(uuid)) {
+                for (Consumer<Profile> consumer : requestedProfilesByUUID.get(uuid)) {
+                    consumer.accept(profile);
                 }
 
-                requestedNames.remove(uuid);
+                requestedProfilesByUUID.remove(uuid);
             }
 
         }
+    }
+
+    @AllArgsConstructor
+    public class Profile {
+
+        @Getter
+        private String name;
+
+        @Getter
+        private UUID uuid;
+
     }
 
 }
